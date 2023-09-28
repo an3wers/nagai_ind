c86654d4aa3ec740cbbc252d9a97fd7b72db0f04
an3wers <an3wer@yandex.ru>
2023-09-28 21:38:07 +0500

    fixed link

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -13,7 +13,8 @@ const AppNavigation = () => {
     (e as MouseEvent).preventDefault();
     const target = document.querySelector(hash);
     if (target) {
-      router.push(hash);
+      // router.push(hash);
+      window.location.hash = hash
       target.scrollIntoView({
         behavior: "smooth",
       });
@@ -24,22 +25,22 @@ const AppNavigation = () => {
     <nav className="absolute md:fixed z-20 top-6 right-6">
       <ul className="flex flex-col md:flex-row gap-6 justify-end">
         <li>
-          <Link href="" onClick={(e: any) => handler(e, "#studio")}>
+          <Link href="#" onClick={(e: any) => handler(e, "#studio")}>
             Studio
           </Link>
         </li>
         <li>
-          <Link href="" onClick={(e: any) => handler(e, "#projects")}>
+          <Link href="#" onClick={(e: any) => handler(e, "#projects")}>
             Projects
           </Link>
         </li>
         <li>
-          <Link href="" onClick={(e: any) => handler(e, "#team")}>
+          <Link href="#" onClick={(e: any) => handler(e, "#team")}>
             Team
           </Link>
         </li>
         <li>
-          <Link href="" onClick={(e: any) => handler(e, "#contact")}>
+          <Link href="#" onClick={(e: any) => handler(e, "#contact")}>
             Contact
           </Link>
         </li>
